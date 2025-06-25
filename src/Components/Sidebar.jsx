216c284bd6bf3d3d98d224e24cc197e66ebfbbd8
Mr-Haseeb786 <m.haseebofficial786@gmail.com>
2025-06-25 10:25:08 +0500

    UI for the settings page done

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,14 +1,9 @@
 import { Search, Settings, User } from "lucide-react";
 import React from "react";
 import { Button } from "./ui/button";
-import { useAppContext } from "@/Context/AppContext";
 import { Link } from "react-router";
 
 const Sidebar = () => {
-  const { theme, setTheme } = useAppContext();
-
-  console.log(theme);
-
   return (
     <aside className="flex flex-col gap-6 align-middle justify-center p-2 accent-bg shadow-2xl sidebar z-10 transition-colors">
       <Link to={"/search"}>
@@ -38,12 +33,6 @@ const Sidebar = () => {
           <Settings className="group-hover:text-white transition-all" />
         </Button>
       </Link>
-
-      <div className="temp flex flex-col">
-        <Button onClick={() => setTheme("midnight")}>Midn</Button>
-        <Button onClick={() => setTheme("charcoal")}>Char</Button>
-        <Button onClick={() => setTheme("noir")}>Noir</Button>
-      </div>
     </aside>
   );
 };
